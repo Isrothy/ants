6cfe7bccfd91d3fac3b79334091788dbe4c31e27
Isrothy <isrothy@gmail.com>
2023-12-07 17:53:32 +0800

    remove laziness

--- src/Model/Document.hs
+++ src/Model/Document.hs
@@ -9,9 +9,9 @@ import qualified Model.Metadata as M
 import Path
 
 data Document = Document
-  { relPath :: Path Rel File,
-    metadata :: M.Metadata,
-    ast :: Maybe MarkdownAst,
-    text :: T.Text
+  { relPath :: !(Path Rel File),
+    metadata :: !M.Metadata,
+    ast :: !(Maybe MarkdownAst),
+    text :: !T.Text
   }
   deriving (Show, Eq)
