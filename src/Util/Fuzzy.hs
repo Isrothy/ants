176408c0402f12f71fd771d43e0a58489d99f738
Isrothy <isrothy@gmail.com>
2024-01-22 20:53:17 +0800

    optimize?

--- src/Util/Fuzzy.hs
+++ src/Util/Fuzzy.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Util.Fuzzy
   ( editDistance,
     minEditDistanceSubstring,
@@ -8,55 +10,92 @@ module Util.Fuzzy
   )
 where
 
+import Control.Loop
+import Control.Monad.ST (runST)
 import qualified Data.Text as T
-import GHC.Arr
+import Data.Text.Internal.Fusion hiding (length)
+import qualified Data.Vector as V
+import qualified Data.Vector.Generic as GV
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as MUV
 
-dp :: (Eq a) => Bool -> [a] -> [a] -> [Int]
-dp allSubstring xs ys = [table ! (m, i) | i <- [0 .. n]]
-  where
-    (m, n) = (length xs, length ys)
-    x = array (1, m) (zip [1 ..] xs)
-    y = array (1, n) (zip [1 ..] ys)
+textToVector :: T.Text -> UV.Vector Char
+textToVector t =
+  case stream t of
+    Stream step s0 _ -> UV.create $ do
+      m <- MUV.new (T.length t)
+      let go s i = case step s of
+            Done -> pure ()
+            Skip s' -> go s' i
+            Yield x s' -> do
+              MUV.write m i x
+              go s' (i + 1)
+      go s0 0
+      pure m
 
-    table :: Array (Int, Int) Int
-    table = array bnds [(ij, dist ij) | ij <- range bnds]
-    bnds = ((0, 0), (m, n))
+min3 :: (Ord a) => a -> a -> a -> a
+min3 a b c = min a (min b c)
 
-    dist (0, j) = if allSubstring then 0 else j
-    dist (i, 0) = i
-    dist (i, j) =
-      minimum
-        [ table ! (i - 1, j) + 1,
-          table ! (i, j - 1) + 1,
-          if x ! i == y ! j then table ! (i - 1, j - 1) else 1 + table ! (i - 1, j - 1)
-        ]
+dp :: (GV.Vector v a, Eq a) => Bool -> v a -> v a -> UV.Vector Int
+dp substrings xs ys = runST $ do
+  let (m, n) = (GV.length xs, GV.length ys)
+  table <- MUV.new ((m + 1) * (n + 1))
+
+  forLoop 0 (<= m) (+ 1) $ \i -> MUV.write table (i * (n + 1)) i
+  forLoop 0 (<= n) (+ 1) $ \j -> MUV.write table j (if substrings then 0 else j)
+
+  forLoop 1 (<= m) (+ 1) $ \i ->
+    forLoop 1 (<= n) (+ 1) $ \j -> do
+      let idx = i * (n + 1) + j
+          diagIdx = (i - 1) * (n + 1) + (j - 1)
+      diag <- MUV.read table diagIdx
+      left <- MUV.read table (idx - 1)
+      up <- MUV.read table (idx - (n + 1))
+      let xChar = xs GV.! (i - 1)
+          yChar = ys GV.! (j - 1)
+          cost = if xChar == yChar then diag else diag + 1
+      MUV.write table idx $ min3 (left + 1) (up + 1) cost
+
+  UV.freeze $ MUV.slice (m * (n + 1)) (n + 1) table
+
+editDistanceV :: (GV.Vector v a, Eq a) => v a -> v a -> Int
+editDistanceV xs ys = UV.last $ dp False xs ys
+
+minEditDistanceSubstringV :: (GV.Vector v a, Eq a) => v a -> v a -> Int
+minEditDistanceSubstringV xs ys = GV.minimum $ dp True xs ys
 
 editDistance :: (Eq a) => [a] -> [a] -> Int
-editDistance xs ys = last $ dp False xs ys
+editDistance xs ys = editDistanceV (V.fromList xs) (V.fromList ys)
 
 minEditDistanceSubstring :: (Eq a) => [a] -> [a] -> Int
-minEditDistanceSubstring xs ys = minimum $ dp True xs ys
+minEditDistanceSubstring xs ys = minEditDistanceSubstringV (V.fromList xs) (V.fromList ys)
 
 threshold :: Int -> Int
 threshold x = floor $ sqrt (fromIntegral x :: Double)
 
+matchesV :: (GV.Vector v a, Eq a) => v a -> v a -> Bool
+matchesV xs ys = editDistanceV xs ys <= threshold (min (GV.length xs) (GV.length ys))
+
+isInfixOfV :: (GV.Vector v a, Eq a) => v a -> v a -> Bool
+isInfixOfV xs ys = minEditDistanceSubstringV xs ys <= threshold (GV.length xs)
+
 matches :: (Eq a) => [a] -> [a] -> Bool
-matches xs ys = editDistance xs ys <= threshold (min (length xs) (length ys))
+matches xs ys = matchesV (V.fromList xs) (V.fromList ys)
 
 isInfixOf :: (Eq a) => [a] -> [a] -> Bool
-isInfixOf xs ys = minEditDistanceSubstring xs ys <= threshold (length xs)
+isInfixOf xs ys = isInfixOfV (V.fromList xs) (V.fromList ys)
 
-preProcess :: T.Text -> String
-preProcess = T.unpack . T.toCaseFold . T.strip
+preProcess :: T.Text -> UV.Vector Char
+preProcess = textToVector . T.toCaseFold . T.strip
 
 matchesT :: T.Text -> T.Text -> Bool
-matchesT xs ys = editDistance xs' ys' <= threshold (min (length xs') (length ys'))
+matchesT xs ys = editDistanceV xs' ys' <= threshold (min (UV.length xs') (UV.length ys'))
   where
     xs' = preProcess xs
     ys' = preProcess ys
 
 isInfixOfT :: T.Text -> T.Text -> Bool
-isInfixOfT xs ys = minEditDistanceSubstring xs' ys' <= threshold (length xs')
+isInfixOfT xs ys = minEditDistanceSubstringV xs' ys' <= threshold (UV.length xs')
   where
     xs' = preProcess xs
     ys' = preProcess ys
