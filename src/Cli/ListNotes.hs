7e870ba22d8bc941ff86afc09615150a04f758bd
q234rty <q23456yuiop@gmail.com>
2024-03-17 17:36:27 +0800

    Change the CLI syntax

--- src/Cli/ListNotes.hs
+++ src/Cli/ListNotes.hs
@@ -6,8 +6,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module Cli.FilterNotes
-  ( filterNotes,
+module Cli.ListNotes
+  ( listNotes,
   )
 where
 
@@ -16,7 +16,7 @@ import Model.Config (getSyntaxSpec)
 import Model.DocQuery (query)
 import Model.Document (Document (relPath, metadata))
 import Parser.DocQuery (completeQuery)
-import Parser.Opts (FilterOptions (..))
+import Parser.Opts (ListOptions (..))
 import Path.IO (getCurrentDir)
 import Path.Posix (toFilePath)
 import Project.DocLoader (loadAllFromDirectory)
@@ -26,23 +26,22 @@ import Text.Parsec.Error (errorMessages, messageString)
 import Data.List (sortBy)
 import qualified Data.Text as T
 import Model.Metadata (Metadata(..))
+import Data.Maybe (fromMaybe)
 comparebyField :: Maybe T.Text -> Document -> Document -> Ordering
 comparebyField x a b = case x of
   Just "title" -> compare (title $ metadata a) (title $ metadata b)
   Just "author" -> compare (author $ metadata a) (author $ metadata b)
   Just "time" -> compare (dateTime $ metadata a) (dateTime $ metadata b)
   _ -> compare (relPath a) (relPath b)
-filterNotes :: FilterOptions -> IO ()
-filterNotes op = do
-  let qs = case queryString op of
-        "" -> "content:/.*/"
-        x -> x
-  let q = parse completeQuery "" $ qs
-  let expr = either (error . concatMap messageString . errorMessages) id q
+listNotes :: ListOptions -> IO ()
+listNotes op = do
   pathToRoot <- fromMaybeM (error "Cannot find config") findRoot
   config <- fromMaybeM (error "config: Decode failed") $ readConfig pathToRoot
   cwd <- getCurrentDir
   docs <- loadAllFromDirectory (getSyntaxSpec config) cwd
+  let qs = fromMaybe "content:/.*/" $ filterString op
+  let q = parse completeQuery "" qs
+  let expr = either (error . concatMap messageString . errorMessages) id q
   let filt = query expr
   let filtered = filter filt docs
   let res = sortBy (comparebyField $ sortString op) filtered
--- src/Parser/Opts.hs
+++ src/Parser/Opts.hs
@@ -9,7 +9,7 @@ module Parser.Opts
     Command (..),
     InitOptions (..),
     NewOptions (..),
-    FilterOptions (..),
+    ListOptions (..),
     LookupTable,
   )
 where
@@ -32,8 +32,8 @@ data InitOptions = InitOptions
   {
   }
 
-data FilterOptions = FilterOptions
-  { queryString :: T.Text,
+data ListOptions = ListOptions
+  { filterString :: Maybe T.Text,
     sortString :: Maybe T.Text
   }
 
@@ -44,7 +44,7 @@ newtype Options = Options
 data Command
   = New NewOptions
   | Init InitOptions
-  | Filter FilterOptions
+  | Lst ListOptions
 
 parsePair :: ReadM (T.Text, T.Text)
 parsePair = eitherReader $ \s ->
@@ -95,14 +95,17 @@ initCommand :: Parser Command
 initCommand = do
   pure $ Init $ InitOptions {}
 
-filterCommand :: Parser Command
-filterCommand = do
+listCommand :: Parser Command
+listCommand = do
   sortString <-
     optional $
       strOption $
         long "sort" <> short 's' <> metavar "FIELD" <> help "Field of metadata used for sorting" <> completeWith ["title", "author", "time"]
-  queryString <- strArgument (metavar "QUERY" <> help "Query to be used for filtering")
-  pure $ Filter $ FilterOptions {..}
+  filterString <-
+    optional $
+      strOption $
+        long "filter" <> short 'f' <> metavar "FILTER" <> help "Expression used for filtering"
+  pure $ Lst $ ListOptions {..}
 
 opts :: Parser Options
 opts = do
@@ -110,6 +113,6 @@ opts = do
     hsubparser
       ( command "init" (info initCommand $ progDesc "Init the notebook")
           <> command "new" (info newCommand $ progDesc "Create a new note")
-          <> command "filter" (info filterCommand $ progDesc "Filter notes")
+          <> command "list" (info listCommand $ progDesc "Filter notes")
       )
   pure $ Options {..}
